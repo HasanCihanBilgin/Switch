ad1ca2a7c456a90f8c0e19d4f035c3f15bad9b95
Hascihan26 <hasancihanbilgin@gmail.com>
2024-08-12 15:10:08 +0300

    update

---
+++ Switch/Switchh/SwitchView.swift
@@ -0,0 +1,68 @@
+//
+//  SwitchView.swift
+//  Switch
+//
+//  Created by Hasan Cihan Bilgin on 12.08.2024.
+//
+
+import SwiftUI
+
+struct SwitchView: View {
+    @State private var isSwitchOn1: Bool = true
+    
+ 
+    @State private var isSwitchOn2: Bool = false
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 20) {
+            Text("Switch")
+                .font(.headline)
+                .padding(.bottom, 10)
+            
+            HStack {
+                VStack(alignment: .leading) {
+                    HStack {
+                        Text("Text")
+                            .font(.body)
+                        Image(systemName: "info.circle")
+                            .foregroundColor(.yellow)
+                    }
+                    Text("Info")
+                        .font(.subheadline)
+                        .foregroundColor(.gray)
+                }
+                Spacer()
+                Toggle("", isOn: $isSwitchOn1)
+                    .toggleStyle(SwitchToggleStyle(tint: .blue))
+            }
+            .padding(.bottom, 10)
+            
+            HStack {
+                VStack(alignment: .leading) {
+                    HStack {
+                        Text("Text")
+                            .font(.body)
+                        Image(systemName: "info.circle")
+                            .foregroundColor(.yellow)
+                    }
+                    Text("Info")
+                        .font(.subheadline)
+                        .foregroundColor(.gray)
+                }
+                Spacer()
+                Toggle("", isOn: $isSwitchOn2)
+                    .toggleStyle(SwitchToggleStyle(tint: .blue))
+            }
+            .padding(.bottom, 10)
+            
+            Spacer()
+        }
+        .padding()
+    }
+}
+
+struct SwitchView_Previews: PreviewProvider {
+    static var previews: some View {
+        SwitchView()
+    }
+}
