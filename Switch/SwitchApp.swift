b57cc19199198cdadb1b072bb0872128ecdeb4da
Hascihan26 <hasancihanbilgin@gmail.com>
2024-08-12 11:45:32 +0300

    Initial Commit

---
+++ Switch/SwitchApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwitchApp.swift
+//  Switch
+//
+//  Created by Hasan Cihan Bilgin on 12.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwitchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
